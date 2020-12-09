393bd0d8876ab5f92a2a97f3b91f327a6951da07
RRSirazhdinov <ruslan.sirazhdinov@mail.ru>
2020-12-10 01:37:49 +0300

    s

--- sandbox/config.py
+++ sandbox/config.py
@@ -6,5 +6,7 @@
 class BaseConfig(object):
     DEBUG = True
     SECRET_KEY = 'secretkey'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///sandbox.db'
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///sandbox.db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost/postgres'
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
