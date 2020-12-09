78c31c80ecd6f1248b32556cf118353a0d3b0583
rrsiraz1 <rrsiraz1@mts.ru>
2020-12-10 00:21:25 +0300

    commit

--- appserver.py
+++ appserver.py
@@ -7,4 +7,3 @@ if __name__ == '__main__':
   from sandbox.application import create_app
   app = create_app()
   app.run(debug=True, host="0.0.0.0", port=8000)
-  # app.run(debug=True, host="127.0.0.1", port=8001)
