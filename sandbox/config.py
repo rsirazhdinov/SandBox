ac59b69d464a68a8db0f92aaa780dc7caff93987
rrsiraz1 <rrsiraz1@mts.ru>
2020-12-10 02:19:46 +0300

    commit

--- sandbox/config.py
+++ sandbox/config.py
@@ -1,3 +1,4 @@
+import os
 """
     config.py
     - settings for the flask application object
@@ -6,5 +7,12 @@
 class BaseConfig(object):
     DEBUG = True
     SECRET_KEY = 'secretkey'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///sandbox.db'
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///sandbox.db'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost/postgres'
+    user = os.environ['SANDBOX_DB_SANDBOX_DB_PG_DB_USER']
+    password = os.environ['SANDBOX_DB_SANDBOX_DB_PG_DB_PASSWORD']
+    host = os.environ['SANDBOX_DB_SANDBOX_DB_PG_SERVICE_NAME']
+    port = os.environ['SANDBOX_DB_SANDBOX_DB_PG_SERVICE_PORT']
+    db_name = os.environ['SANDBOX_DB_SANDBOX_DB_PG_DB_NAME']
+    SQLALCHEMY_DATABASE_URI = 'postgresql://{}:{}@{}:{}/{}'.format(user, password, host, port, db_name)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
