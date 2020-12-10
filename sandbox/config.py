a10dac3efe9c0460f9270ad1140ffd025abd13ba
rrsiraz1 <rrsiraz1@mts.ru>
2020-12-11 00:31:16 +0300

    commit

--- sandbox/config.py
+++ sandbox/config.py
@@ -9,10 +9,10 @@ class BaseConfig(object):
     SECRET_KEY = 'secretkey'
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///sandbox.db'
     # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost/postgres'
-    user = os.getenv('SANDBOX_DB_SANDBOX_DB_PG_DB_USER','4eddfa95-3999-4ed9-a1ef-6e40c0da040e')
-    password = os.getenv('SANDBOX_DB_SANDBOX_DB_PG_DB_PASSWORD','bwv11kp5lru3dthm6j2cnwwmt')
+    user = os.getenv('SANDBOX_DB_SANDBOX_DB_PG_DB_USER','38ef6bbf-f9ce-4154-8eb5-cf1c9ceea057')
+    password = os.getenv('SANDBOX_DB_SANDBOX_DB_PG_DB_PASSWORD','b5c5u2cbldnoj3vg182bfiqmy')
     host = os.getenv('SANDBOX_DB_SANDBOX_DB_PG_SERVICE_NAME','0700ngenie-infr-solut-dbp.ngenie.mtsit.com')
     port = os.getenv('SANDBOX_DB_SANDBOX_DB_PG_SERVICE_PORT', 5432)
-    db_name = os.getenv('SANDBOX_DB_SANDBOX_DB_PG_DB_NAME','1f3e7ed3-110f-4745-9f01-1f9ba1878ace')
+    db_name = os.getenv('SANDBOX_DB_SANDBOX_DB_PG_DB_NAME','b4b7a530-dd26-4af9-ab38-0dccb91b16c7')
     SQLALCHEMY_DATABASE_URI = 'postgresql://{}:{}@{}:{}/{}'.format(user, password, host, port, db_name)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
