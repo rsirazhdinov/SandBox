067100edd2c649da83f790c4aa19335dd96c2727
rrsiraz1 <rrsiraz1@mts.ru>
2020-12-24 14:20:55 +0300

    сертификаты

--- appserver.py
+++ appserver.py
@@ -5,5 +5,6 @@ appserver.py
 if __name__ == '__main__':
   from sandbox.application import create_app
   app = create_app()
+  app.config['CORS_HEADERS'] = 'Content-Type'
   context = ('ssl/bapi-sandbox.mts.ru.cer', 'ssl/bapi-sandbox.mts.ru.key')
   app.run(debug=True, host="0.0.0.0", port=8000, ssl_context=context)
